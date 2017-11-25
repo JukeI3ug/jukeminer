1400d40e733a61fbde59a517992ccd8292e76730
Matthew Culmone <matt.culmone@vividcortex.com>
2017-11-25 15:01:03 -0500

    Update update.sh

--- scripts/update.sh
+++ scripts/update.sh
@@ -6,3 +6,4 @@ git -C /opt/download/jukeminer pull origin master
 
 #copy downloaded files to their destinations
 cp /opt/download/jukeminer/fan/* /opt/fan/
+cp /opt/download/jukeminer/scripts/* /opt/scripts/
