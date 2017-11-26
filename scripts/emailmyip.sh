37dc607d6e1f4da95a029f09df4668678f0d4cfd
Matthew Culmone <matt.culmone@vividcortex.com>
2017-11-25 21:57:55 -0500

    Update emailmyip.sh
    Changed path reference to IP check to be fixed and not relative

--- scripts/emailmyip.sh
+++ scripts/emailmyip.sh
@@ -3,7 +3,7 @@ set -x
 #set local v IPADDRESS to the value parsed out of ifconfig; currently using enp5s0 as the interface
 IPADDRESS=$(/sbin/ifconfig enp5s0 | sed -n 's/.*inet addr:\([^ ]*\).*/\1/p')
 #check if the ip has changed
-if [[ "${IPADDRESS}" != $(cat ~/.current_ip) ]]
+if [[ "${IPADDRESS}" != $(cat /home/miner/.current_ip) ]]
 then
 #if it has then send an email
 echo "To: keymaster@iron-key.com
@@ -11,5 +11,5 @@ From: bcwc.crypto@gmail.com
 Subject: Mining Rig IP Updated
 Your new IP address is ${IPADDRESS}" | ssmtp keymaster@iron-key.com
 #update the IP address into a file in the user's home directory
-echo ${IPADDRESS} >|~/.current_ip
+echo ${IPADDRESS} >|/home/miner/.current_ip
 fi
